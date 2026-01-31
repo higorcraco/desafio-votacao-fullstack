e808030eeb69f7757488cad558591b45342dd84e
Higor Craco <higorcraco@gmail.com>
2026-01-30 22:53:12 -0300

    Projeto criado com spring initializer

---
+++ backend/src/test/java/com/higorcraco/votacao_fullstack/VotacaoFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.higorcraco.votacao_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
